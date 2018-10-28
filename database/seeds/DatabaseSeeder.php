4066e4ecdf0ed007db9723efb9729f0cd272f436
Joesdev <joesilvadev@gmail.com>
2018-10-28 06:57:43 +0000

    Added a seeder and factory for pages table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserHierarchySeeder::class);
         $this->call(ActionsTableSeeder::class);
+        $this->call(PagesTableSeeder::class);
     }
 }
