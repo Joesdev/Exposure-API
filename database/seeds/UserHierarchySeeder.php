c09f2e3dc703f69efbb45950e49dd2ba0f606bd7
Joesdev <joesilvadev@gmail.com>
2018-10-28 05:53:10 +0000

    Added logic to UserHierarchySeeder to create two related hierarchies per user

--- database/seeds/UserHierarchySeeder.php
+++ database/seeds/UserHierarchySeeder.php
@@ -13,10 +13,20 @@ class UserHierarchySeeder extends Seeder
      */
     public function run()
     {
+        // Create 5 Users, Each Owning a Single Hierarchy
         factory(User::class, 5)->create()->each(function ($u){
             $u->hierarchies()->save(factory(Hierarchy::class)->make([
                 'user_id' => $u->id
             ]));
+        });
+
+        // Create 2 Users, Each Owning Two Hierarchies
+        factory(User::class, 2)->create()->each(function ($u){
+            for($count = 0; $count < 2; $count++){
+                $u->hierarchies()->save(factory(Hierarchy::class)->make([
+                    'user_id' => $u->id
+                ]));
+            }
         });
     }
 }
