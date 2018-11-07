f919531f246ebdc225f5e1e9b21d1bbaff78d275
Joesdev <joesilvadev@gmail.com>
2018-11-07 07:34:49 +0000

    added test function test_index_returns_multiple_hierarchies_for_a_user

--- tests/Unit/HierarchyControllerTest.php
+++ tests/Unit/HierarchyControllerTest.php
@@ -13,29 +13,32 @@ class HierarchyControllerTest extends TestCase
     use RefreshDatabase;
 
     protected $user;
+    protected $num_hierarchies = 3;
 
     public function setUp()
     {
         parent::setUp();
         $this->user = factory(User::class)->create();
-        factory(Hierarchy::class, 2)->create(['user_id' => $this->user->id]);
+        factory(Hierarchy::class, $this->num_hierarchies)->create(['user_id' => $this->user->id]);
     }
 
-    /*public function test_index_returns_multiple_hierarchies_for_a_user()
+    public function test_index_returns_multiple_hierarchies_for_a_user()
     {
         $response = $this->actingAs($this->user)
                         ->json('GET','/api/hierarchy');
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals(2, count($response->getOriginalContent()));
-    }*/
+        $this->assertEquals($this->num_hierarchies, count($response->getOriginalContent()));
+    }
 
     public function test_show_returns_a_single_hierarchy_based_on_an_id()
     {
-        dd(User::all());
         $hierarchy_id = 2;
         $response = $this->actingAs($this->user)
                         ->json('GET','/api/hierarchy/' . $hierarchy_id);
         $this->assertEquals(200, $response->getStatusCode());
-        $response->assertJsonStructure(['user_id', 'goal']);
+        $response->assertJsonStructure([
+            'user_id',
+            'goal'
+        ]);
     }
 }
