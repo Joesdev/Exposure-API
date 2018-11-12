e7988b7477bce8fd896e708fb444d717a90cdc44
Joesdev <joesilvadev@gmail.com>
2018-11-12 07:47:42 +0000

    Added test functions for addAction

--- app/Hierarchy.php
+++ app/Hierarchy.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class Hierarchy extends Model
 {
@@ -17,8 +18,19 @@ class Hierarchy extends Model
         return $this->hasMany('App\Action');
     }
 
-    public function addAction($input_fields)
+    public function addAction($action)
     {
-        $this->actions()->create($input_fields);
+        if($action instanceof Action){
+            return $this->actions()->save($action);
+        }else if($action instanceof Collection){
+            return $this->actions()->saveMany($action);
+        } else{ //Assoc Array
+            $this->actions()->create($action);
+        }
+    }
+
+    public function countActions()
+    {
+        return $this->actions()->count();
     }
 }
---
+++ tests/Feature/HierarchyTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Action;
+use App\Hierarchy;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class HierarchyTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_addAction_adds_a_row_to_actions_table_related_to_a_hierarchy_using_assoc_array()
+    {
+        $hierarchy = factory(Hierarchy::class)->create();
+        $hierarchy->addAction([
+            'level' => random_int(1,10),
+            'description' => $this->faker->sentence(6,false)
+        ]);
+        $this->assertEquals(1, $hierarchy->countActions());
+    }
+
+    public function test_addAction_adds_a_row_to_actions_table_using_action_class()
+    {
+        $hierarchy = factory(Hierarchy::class)->create();
+        $action = factory(Action::class,1)->create();
+        $hierarchy->addAction($action);
+        $this->assertEquals(1, $hierarchy->countActions());
+
+        $action2 = factory(Action::class,1)->create();
+        $hierarchy->addAction($action2);
+        $this->assertEquals(2, $hierarchy->countActions());
+    }
+
+    public function test_addAction_adds_rows_to_database_using_collection_of_actions()
+    {
+        $hierarchy = factory(Hierarchy::class)->create();
+        $actions = factory(Action::class,4)->create();
+        $hierarchy->addAction($actions);
+        $this->assertEquals(4, $hierarchy->countActions());
+    }
+
+
+}
