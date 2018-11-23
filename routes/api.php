4381acc7c141174ea95a4c4f0553c794510afe86
Joesdev <joesilvadev@gmail.com>
2018-11-23 06:20:19 +0000

    Moved delete and update to api.php

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,6 @@ Route::get('/hierarchy', 'HierarchyController@index')->name('hierarchy.index');
 Route::post('/hierarchy', 'HierarchyController@store')->name('hierarchy.store');
 Route::delete('/hierarchy/{hierarchy}', 'HierarchyController@destroy')->name('hierarchy.delete');
 Route::patch('/hierarchy/{hierarchy}', 'HierarchyController@update')->name('hierarchy.update');
-
 Route::get('/hierarchy/{hierarchy}', 'HierarchyController@show')->name('hierarchy.show');
 Route::get('/hierarchy/{hierarchy}/actions', 'HierarchyController@actions')->name('hierarchy.actions');
 
