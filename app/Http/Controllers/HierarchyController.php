ebee3a20b20790755a4c9b4750fff72f0698c672
Joseph Silva <joesilvadev@gmail.com>
2018-12-11 03:51:40 +0000

    Removed edit function from hierarchycontroller

--- app/Http/Controllers/HierarchyController.php
+++ app/Http/Controllers/HierarchyController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\Hierarchy as HierarchyResource;
 use App\Hierarchy;
 
 class HierarchyController extends Controller
@@ -17,30 +17,25 @@ class HierarchyController extends Controller
     public function store()
     {
         $hierarchy = Hierarchy::create(request()->all());
-        return $hierarchy;
+        return new HierarchyResource($hierarchy);
     }
 
     public function destroy(Hierarchy $hierarchy)
     {
         if($hierarchy->delete()){
-            return $hierarchy;
+            return new HierarchyResource($hierarchy);
         }
     }
 
     public function update(Hierarchy $hierarchy)
     {
         $hierarchy->update(request()->all());
-        return $hierarchy;
+        return new HierarchyResource($hierarchy);
     }
 
     public function show(Hierarchy $hierarchy)
     {
-        return $hierarchy;
-    }
-
-    public function edit(Hierarchy $hierarchy)
-    {
-        return view('test_forms.hierarchy_edit', compact('hierarchy'));
+        return new HierarchyResource($hierarchy);
     }
 
     public function actions(Hierarchy $hierarchy)
